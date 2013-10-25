5335e12ae8494d55fa158e080b7353557b549588
Brandon John-Freso <brandonjf@atomicflowtech.com>
2013-10-25 12:50:25 -0400

    Commiting some listview changes.

--- js/controllers/userAiListCtrl.js
+++ js/controllers/userAiListCtrl.js
@@ -1,5 +1,6 @@
 euclidAV.controller("UserAiListCtrl", function($scope, aiService){
   $scope.openTest = "Open TEST";
+  
 
   //$scope.toggleBookmark = function(actionItem){aiService.toggleBookmark(actionItem);}
   //window.UserAiListCtrl = $scope;
